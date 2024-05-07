51daa1081b305face222f1cb13a9f8b87df3a520
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-05-08 00:20:00 +0200

    Fix lightboxImage default alternative text [Accessibility]

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -215,7 +215,7 @@
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid"/>
+                            <img class="lightboxImage img-fluid" alt="Lightbox default"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
